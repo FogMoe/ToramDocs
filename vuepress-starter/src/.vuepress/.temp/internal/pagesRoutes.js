dd6777fb2d3bdb3e91be353f75f59410bb725827
Kc <kc@fog.moe>
2024-02-03 13:23:23 +0800

    vuepress

---
+++ vuepress-starter/src/.vuepress/.temp/internal/pagesRoutes.js
@@ -0,0 +1,5 @@
+export const pagesRoutes = [
+  ["v-6c0089f0","/get-started.html",{"title":"Get Started"},[":md"]],
+  ["v-8daa1a0e","/",{"title":"Home"},["/README.md"]],
+  ["v-3706649a","/404.html",{"title":""},[]],
+]
