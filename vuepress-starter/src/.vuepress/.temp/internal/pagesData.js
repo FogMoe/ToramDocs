9c69eb999bbb371e50554937b016911638f5b042
Kc <kc@fog.moe>
2024-02-03 13:25:49 +0800

    2

--- vuepress-starter/src/.vuepress/.temp/internal/pagesData.js
+++
@@ -1,8 +0,0 @@
-export const pagesData = {
-  // path: /get-started.html
-  "v-6c0089f0": () => import(/* webpackChunkName: "v-6c0089f0" */"D:/ToramDocs/vuepress-starter/src/.vuepress/.temp/pages/get-started.html.js").then(({ data }) => data),
-  // path: /
-  "v-8daa1a0e": () => import(/* webpackChunkName: "v-8daa1a0e" */"D:/ToramDocs/vuepress-starter/src/.vuepress/.temp/pages/index.html.js").then(({ data }) => data),
-  // path: /404.html
-  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"D:/ToramDocs/vuepress-starter/src/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
-}
--- vuepress-starter/src/.vuepress/.temp/internal/pagesRoutes.js
+++
@@ -1,5 +0,0 @@
-export const pagesRoutes = [
-  ["v-6c0089f0","/get-started.html",{"title":"Get Started"},[":md"]],
-  ["v-8daa1a0e","/",{"title":"Home"},["/README.md"]],
-  ["v-3706649a","/404.html",{"title":""},[]],
-]
