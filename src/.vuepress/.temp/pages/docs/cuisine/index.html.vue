200c7c0c8d632439713e74abdb80e881c8338606
Kc <kc@fog.moe>
2024-02-04 03:47:43 +0800

    1

--- src/.vuepress/.temp/pages/docs/cuisine/index.html.vue
+++ src/.vuepress/.temp/pages/docs/cuisine/index.html.vue
@@ -1,6 +1,7 @@
 <template><div><h1 id="料理地址" tabindex="-1"><a class="header-anchor" href="#料理地址"><span>料理地址</span></a></h1>
 <h2 id="托蘭異世錄" tabindex="-1"><a class="header-anchor" href="#托蘭異世錄"><span>托蘭異世錄</span></a></h2>
-<h3 id="hp上限" tabindex="-1"><a class="header-anchor" href="#hp上限"><span>HP上限</span></a></h3>
+<h3 id="「基礎類」" tabindex="-1"><a class="header-anchor" href="#「基礎類」"><span>「基礎類」</span></a></h3>
+<h4 id="hp上限" tabindex="-1"><a class="header-anchor" href="#hp上限"><span>HP上限</span></a></h4>
 <ul>
 <li>1140001</li>
 <li>1250000</li>
@@ -12,17 +13,197 @@
 <li>1010417</li>
 <li>3040005</li>
 </ul>
-<h3 id="mp上限" tabindex="-1"><a class="header-anchor" href="#mp上限"><span>MP上限</span></a></h3>
-<h3 id="暴擊率" tabindex="-1"><a class="header-anchor" href="#暴擊率"><span>暴擊率</span></a></h3>
-<h3 id="攻回" tabindex="-1"><a class="header-anchor" href="#攻回"><span>攻回</span></a></h3>
-<h3 id="減仇" tabindex="-1"><a class="header-anchor" href="#減仇"><span>減仇</span></a></h3>
-<h3 id="增仇" tabindex="-1"><a class="header-anchor" href="#增仇"><span>增仇</span></a></h3>
-<h3 id="atk" tabindex="-1"><a class="header-anchor" href="#atk"><span>ATK</span></a></h3>
-<h3 id="matk" tabindex="-1"><a class="header-anchor" href="#matk"><span>MATK</span></a></h3>
-<h3 id="武器atk" tabindex="-1"><a class="header-anchor" href="#武器atk"><span>武器ATK</span></a></h3>
-<h3 id="物理抗性" tabindex="-1"><a class="header-anchor" href="#物理抗性"><span>物理抗性</span></a></h3>
-<h3 id="魔法抗性" tabindex="-1"><a class="header-anchor" href="#魔法抗性"><span>魔法抗性</span></a></h3>
+<h4 id="mp上限" tabindex="-1"><a class="header-anchor" href="#mp上限"><span>MP上限</span></a></h4>
+<ul>
+<li>1240000</li>
+<li>4120003</li>
+<li>1027777</li>
+<li>1017676</li>
+<li>1020808</li>
+<li>5236969</li>
+<li>2011234</li>
+<li>1091111</li>
+<li>3040002</li>
+</ul>
+<h4 id="暴擊率" tabindex="-1"><a class="header-anchor" href="#暴擊率"><span>暴擊率</span></a></h4>
+<ul>
+<li>7162029</li>
+<li>1037777</li>
+<li>2269030</li>
+<li>1010403</li>
+</ul>
+<h4 id="攻回" tabindex="-1"><a class="header-anchor" href="#攻回"><span>攻回</span></a></h4>
+<ul>
+<li>1160001</li>
+<li>5010031</li>
+<li>4261111</li>
+<li>1111000</li>
+<li>3040003</li>
+<li>4206969</li>
+<li>4083005</li>
+<li>1111314</li>
+</ul>
+<h4 id="命中" tabindex="-1"><a class="header-anchor" href="#命中"><span>命中</span></a></h4>
+<ul>
+<li>4261111</li>
+</ul>
+<h4 id="迴避" tabindex="-1"><a class="header-anchor" href="#迴避"><span>迴避</span></a></h4>
+<ul>
+<li>2010408</li>
+</ul>
+<h3 id="「仇恨類」" tabindex="-1"><a class="header-anchor" href="#「仇恨類」"><span>「仇恨類」</span></a></h3>
+<h4 id="減仇" tabindex="-1"><a class="header-anchor" href="#減仇"><span>減仇</span></a></h4>
+<ul>
+<li>1010038</li>
+<li>1010147</li>
+<li>1030521</li>
+<li>4087777</li>
+</ul>
+<h4 id="增仇" tabindex="-1"><a class="header-anchor" href="#增仇"><span>增仇</span></a></h4>
+<ul>
+<li>1140002</li>
+<li>1011340</li>
+</ul>
+<h3 id="「攻擊防禦類」" tabindex="-1"><a class="header-anchor" href="#「攻擊防禦類」"><span>「攻擊防禦類」</span></a></h3>
+<h4 id="atk" tabindex="-1"><a class="header-anchor" href="#atk"><span>ATK</span></a></h4>
+<p>暫無收錄</p>
+<h4 id="matk" tabindex="-1"><a class="header-anchor" href="#matk"><span>MATK</span></a></h4>
+<p>暫無收錄</p>
+<h4 id="武器atk" tabindex="-1"><a class="header-anchor" href="#武器atk"><span>武器ATK</span></a></h4>
+<ul>
+<li>5110834</li>
+<li>3160777</li>
+</ul>
+<h4 id="物理抗性" tabindex="-1"><a class="header-anchor" href="#物理抗性"><span>物理抗性</span></a></h4>
+<ul>
+<li>1020001</li>
+<li>1030777</li>
+<li>7010014</li>
+</ul>
+<h4 id="魔法抗性" tabindex="-1"><a class="header-anchor" href="#魔法抗性"><span>魔法抗性</span></a></h4>
+<ul>
+<li>1010004</li>
+<li>10123456</li>
+<li>7190000</li>
+</ul>
+<h4 id="物理防禦" tabindex="-1"><a class="header-anchor" href="#物理防禦"><span>物理防禦</span></a></h4>
+<p>暫無收錄</p>
+<h4 id="魔法防禦" tabindex="-1"><a class="header-anchor" href="#魔法防禦"><span>魔法防禦</span></a></h4>
+<p>暫無收錄</p>
+<h3 id="「能力類」" tabindex="-1"><a class="header-anchor" href="#「能力類」"><span>「能力類」</span></a></h3>
+<p>暫無收錄</p>
+<h3 id="「對屬抗屬類」" tabindex="-1"><a class="header-anchor" href="#「對屬抗屬類」"><span>「對屬抗屬類」</span></a></h3>
+<p>暫無收錄</p>
+<h3 id="「其他類」" tabindex="-1"><a class="header-anchor" href="#「其他類」"><span>「其他類」</span></a></h3>
+<p>暫無收錄</p>
 <h2 id="托拉姆物語" tabindex="-1"><a class="header-anchor" href="#托拉姆物語"><span>托拉姆物語</span></a></h2>
+<h3 id="「基礎類」-1" tabindex="-1"><a class="header-anchor" href="#「基礎類」-1"><span>「基礎類」</span></a></h3>
+<h4 id="hp上限-1" tabindex="-1"><a class="header-anchor" href="#hp上限-1"><span>HP上限</span></a></h4>
+<ul>
+<li>1079999</li>
+<li>1010139</li>
+<li>1010000</li>
+<li>1040301</li>
+<li>6171234</li>
+</ul>
+<h4 id="mp上限-1" tabindex="-1"><a class="header-anchor" href="#mp上限-1"><span>MP上限</span></a></h4>
+<ul>
+<li>1010014</li>
+<li>1011080</li>
+<li>1010555</li>
+<li>5205277</li>
+<li>4010556</li>
+<li>4221633</li>
+<li>1109999</li>
+<li>1010522</li>
+<li>1259527</li>
+</ul>
+<h4 id="暴擊率-1" tabindex="-1"><a class="header-anchor" href="#暴擊率-1"><span>暴擊率</span></a></h4>
+<ul>
+<li>1010928</li>
+<li>1010037</li>
+<li>1010015</li>
+<li>1029527</li>
+<li>1030007</li>
+<li>1011777</li>
+<li>1010500</li>
+<li>4237777</li>
+<li>1011020</li>
+<li>1019763</li>
+<li>1012000</li>
+<li>7240520</li>
+</ul>
+<h4 id="攻回-1" tabindex="-1"><a class="header-anchor" href="#攻回-1"><span>攻回</span></a></h4>
+<ul>
+<li>1010567</li>
+<li>1260520</li>
+<li>1010161</li>
+<li>1011985</li>
+<li>6261021</li>
+<li>6010004</li>
+<li>7240520</li>
+<li>1010144</li>
+</ul>
+<h4 id="命中-1" tabindex="-1"><a class="header-anchor" href="#命中-1"><span>命中</span></a></h4>
+<p>暫無收錄</p>
+<h4 id="迴避-1" tabindex="-1"><a class="header-anchor" href="#迴避-1"><span>迴避</span></a></h4>
+<p>暫無收錄</p>
+<h3 id="「仇恨類」-1" tabindex="-1"><a class="header-anchor" href="#「仇恨類」-1"><span>「仇恨類」</span></a></h3>
+<h4 id="減仇-1" tabindex="-1"><a class="header-anchor" href="#減仇-1"><span>減仇</span></a></h4>
+<ul>
+<li>1010018</li>
+<li>1030008</li>
+<li>1010993</li>
+<li>1010399</li>
+</ul>
+<h4 id="增仇-1" tabindex="-1"><a class="header-anchor" href="#增仇-1"><span>增仇</span></a></h4>
+<ul>
+<li>3060000</li>
+<li>6010004</li>
+<li>1020016</li>
+</ul>
+<h3 id="「攻擊防禦類」-1" tabindex="-1"><a class="header-anchor" href="#「攻擊防禦類」-1"><span>「攻擊防禦類」</span></a></h3>
+<h4 id="atk-1" tabindex="-1"><a class="header-anchor" href="#atk-1"><span>ATK</span></a></h4>
+<p>暫無收錄</p>
+<h4 id="matk-1" tabindex="-1"><a class="header-anchor" href="#matk-1"><span>MATK</span></a></h4>
+<p>暫無收錄</p>
+<h4 id="武器atk-1" tabindex="-1"><a class="header-anchor" href="#武器atk-1"><span>武器ATK</span></a></h4>
+<ul>
+<li>1010106</li>
+<li>2020202</li>
+<li>1012580</li>
+<li>1010038</li>
+<li>1010321</li>
+</ul>
+<h4 id="物理抗性-1" tabindex="-1"><a class="header-anchor" href="#物理抗性-1"><span>物理抗性</span></a></h4>
+<ul>
+<li>1012200</li>
+<li>1231230</li>
+<li>5076666</li>
+</ul>
+<h4 id="魔法抗性-1" tabindex="-1"><a class="header-anchor" href="#魔法抗性-1"><span>魔法抗性</span></a></h4>
+<ul>
+<li>1010074</li>
+<li>1019764</li>
+<li>1052100</li>
+<li>4010555</li>
+<li>1010228</li>
+<li>5076665</li>
+</ul>
+<h4 id="物理防禦-1" tabindex="-1"><a class="header-anchor" href="#物理防禦-1"><span>物理防禦</span></a></h4>
+<p>暫無收錄</p>
+<h4 id="魔法防禦-1" tabindex="-1"><a class="header-anchor" href="#魔法防禦-1"><span>魔法防禦</span></a></h4>
+<p>暫無收錄</p>
+<h3 id="「能力類」-1" tabindex="-1"><a class="header-anchor" href="#「能力類」-1"><span>「能力類」</span></a></h3>
+<h4 id="力量" tabindex="-1"><a class="header-anchor" href="#力量"><span>力量</span></a></h4>
+<ul>
+<li>2010151</li>
+<li>4266666</li>
+</ul>
+<h3 id="「對屬抗屬類」-1" tabindex="-1"><a class="header-anchor" href="#「對屬抗屬類」-1"><span>「對屬抗屬類」</span></a></h3>
+<p>暫無收錄</p>
+<h3 id="「其他類」-1" tabindex="-1"><a class="header-anchor" href="#「其他類」-1"><span>「其他類」</span></a></h3>
+<p>暫無收錄</p>
 </div></template>
 
 
